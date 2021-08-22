cdd9bd9f621ec6b435b7eb770fc8b48adafeccc6
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-08-22 19:58:50 +0200

    tidying up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 vim.api.nvim_exec(
@@ -17,34 +17,32 @@ false
 
 local use = require('packer').use
 require('packer').startup(function()
-	use 'wbthomason/packer.nvim' -- Package manager
-	use 'pineapplegiant/spaceduck' -- Color scheme 
-	use 'itchyny/lightline.vim' -- Fancier statusline
-	use {'kristijanhusak/orgmode.nvim', config = function()
-		require('orgmode').setup{}
-	end
-}
-use 'folke/tokyonight.nvim'
-use {
-	'nvim-telescope/telescope.nvim',
-	requires = { {'nvim-lua/plenary.nvim'} }
-}
-use {
-	'nvim-treesitter/nvim-treesitter',
-	run = ':TSUpdate'
-}
-use 'neovim/nvim-lspconfig'
-use 'hrsh7th/nvim-compe' -- Autocompletion plugin
-use 'L3MON4D3/LuaSnip' -- Snippets plugin
-use 'junegunn/goyo.vim'
-use 'godlygeek/tabular'
-use 'plasticboy/vim-markdown'
-use {
-	'romgrk/barbar.nvim',
-	requires = {'kyazdani42/nvim-web-devicons'}
-}
-end)
-
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'pineapplegiant/spaceduck' -- Color scheme 
+  use 'itchyny/lightline.vim' -- Fancier statusline
+  use {'kristijanhusak/orgmode.nvim', config = function()
+    require('orgmode').setup{}
+  end 
+  }
+  use 'folke/tokyonight.nvim'
+  use {'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe' -- Autocompletion plugin
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'junegunn/goyo.vim'
+  use 'godlygeek/tabular'
+  use 'plasticboy/vim-markdown'
+  use {
+    'romgrk/barbar.nvim',
+    requires = {'kyazdani42/nvim-web-devicons'}
+  }
+  end
+)
 
 require('lsp_config')
 
@@ -59,6 +57,8 @@ vim.g.tokyonight_style = "night"
 vim.o.ruler = false
 vim.o.laststatus = 0
 vim.o.mouse = 'a'
+
+-- Spaces instead of tabs
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
@@ -66,9 +66,9 @@ vim.o.softtabstop = 2
 
 --Set statusbar
 vim.g.lightline = {
-	colorscheme = 'tokyonight',
-	active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
-	component_function = { gitbranch = 'fugitive#head' },
+  colorscheme = 'tokyonight',
+  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
+  component_function = { gitbranch = 'fugitive#head' },
 }
 
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
@@ -106,30 +106,30 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- Compe setup
 require('compe').setup {
-	source = {
-		path = true,
-		nvim_lsp = true,
-		luasnip = true,
-		buffer = false,
-		calc = false,
-		nvim_lua = false,
-		vsnip = false,
-		ultisnips = false,
-	},
+  source = {
+    path = true,
+    nvim_lsp = true,
+    luasnip = true,
+    buffer = false,
+    calc = false,
+    nvim_lua = false,
+    vsnip = false,
+    ultisnips = false,
+  },
 }
 
 -- Utility functions for compe and luasnip
 local t = function(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
-	local col = vim.fn.col '.' - 1
-	if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
-		return true
-	else
-		return false
-	end
+  local col = vim.fn.col '.' - 1
+  if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
+    return true
+  else
+    return false
+  end
 end
 
 -- Use (s-)tab to:
@@ -138,25 +138,25 @@ end
 local luasnip = require 'luasnip'
 
 _G.tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t '<C-n>'
-	elseif luasnip.expand_or_jumpable() then
-		return t '<Plug>luasnip-expand-or-jump'
-	elseif check_back_space() then
-		return t '<Tab>'
-	else
-		return vim.fn['compe#complete']()
-	end
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-n>'
+  elseif luasnip.expand_or_jumpable() then
+    return t '<Plug>luasnip-expand-or-jump'
+  elseif check_back_space() then
+    return t '<Tab>'
+  else
+    return vim.fn['compe#complete']()
+  end
 end
 
 _G.s_tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t '<C-p>'
-	elseif luasnip.jumpable(-1) then
-		return t '<Plug>luasnip-jump-prev'
-	else
-		return t '<S-Tab>'
-	end
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-p>'
+  elseif luasnip.jumpable(-1) then
+    return t '<Plug>luasnip-jump-prev'
+  else
+    return t '<S-Tab>'
+  end
 end
 
 -- Map tab to the above tab complete functiones
