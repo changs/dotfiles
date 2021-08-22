a8e45fb9abfc371d0d4a320d1dab647bde0734c9
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-08-22 16:05:05 +0200

    Change tabs to spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -59,6 +59,10 @@ vim.g.tokyonight_style = "night"
 vim.cmd [[set noruler]]
 vim.cmd [[set laststatus=0]]
 vim.o.mouse = 'a'
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
 
 --Set statusbar
 vim.g.lightline = {
